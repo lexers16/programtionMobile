82cba5bcb2b4309dae52988db01ddf0aa957f918
Alexendre <alexendre.o@outlook.fr>
2019-12-13 15:35:11 +0100

    api

--- ProgMobile/api.swift
+++ ProgMobile/api.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+import Alamofire
+
+var appKey : String!
+
+class apiLooker {
+    
+    func getArtists (completion: @escaping (NSDictionary)->()) {
+        let headers: HTTPHeaders = [
+          "Authorization":"Bearer \(appKey!)"
+        ]
+        AF.request("https://api.spotify.com/v1/browse/new-releases", headers: headers)
+            .responseJSON { response in
+                
+                switch response.result {
+
+                case .success(let json):
+                    let JSON = json as? NSDictionary
+                    let id = JSON!["albums"] as? NSDictionary
+                    
+                        completion(id!)
+                    
+                case .failure(let error):
+                    print(error)
+                }
+                }
+    }
+    
+    func getToken (completion: @escaping (Bool)->()) {
+        let headers: HTTPHeaders = [
+          "Authorization":"Basic NmY4YzJlODczM2RkNDYxMzlkNDk1NTU2OWM1ZTM4YjY6NDAxNDhkN2M2NDkwNDNlNTlhOTM0MmZhZTEzMDRjMjY="
+        ]
+        AF.request("https://accounts.spotify.com/api/token", method: .post, parameters: ["grant_type": "client_credentials"], headers: headers)
+            .responseJSON { response in
+            
+            switch response.result {
+
+            case .success(let json):
+                let JSON = json as? NSDictionary
+                appKey = JSON!["access_token"] as! String
+                
+                    completion(true)
+                
+            case .failure(let error):
+                print(error)
+            }
+            }
+    }
+}
